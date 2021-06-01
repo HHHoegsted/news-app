7bd704c2283b09309d12ccfb1a36f9a8f82be75a
HHHoegsted <hh.hoegsted@gmail.com>
2021-06-01 10:27:17 +0200

    ADD: Added migration, factory and seeder for the Article class

---
+++ src/database/factories/ArticleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Bluemmb\Faker\PicsumPhotosProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+      $this->faker->addProvider(new PicsumPhotosProvider($this->faker));
+        return [
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->paragraphs(5),
+            'image_url' => $this->faker->imageUrl(),
+        ];
+    }
+}
